b908776d31806608410169c3f22d7b033ae1d86b
MartinCastroAlvarez <you@example.com>
2017-12-28 19:17:46 -0300

    fix

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,7 @@ clear
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ ! -d "$DIR/../env/" ]
 then
-	virtualenv -p python3 $DIR/../env
+	virtualenv -p python3 $DIR/../.env
 fi
 cd $DIR/../
 source .env/bin/activate
