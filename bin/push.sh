f8e5c8fe93cec7cc3c77779d7a1c5641031234e4
MartinCastroAlvarez <you@example.com>
2017-12-31 02:08:01 -0300

    fix

--- bin/push.sh
+++ bin/push.sh
@@ -3,6 +3,8 @@ branch=$(git branch | awk '{print $2}')
 echo "Pushing your code to $branch..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
-git add * .*ignore .circleci/
+git add * 2> /dev/null
+git add .*ignore 2> /dev/null
+git add .circleci/ 2> /dev/null
 git commit -a -m "fix"
 git push origin $branch
