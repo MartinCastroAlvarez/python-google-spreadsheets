24dd3a9ce9d006b1205b14e62b2518b345a669f3
MartinCastroAlvarez <you@example.com>
2018-01-05 18:48:31 -0300

    fix

--- bin/test.sh
+++ bin/test.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# Initializing...
-echo "Running py.tests..."
-
 # Activate virtual environment.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/env.sh
 
+# Determine the type of tests to run.
+TESTS=$1
+if [ $TESTS == "QA" ]
+then
+    echo "Running functional tests..."
+    FILENAME="qa.py"
+else
+    echo "Running unit tests..."
+    FILENAME="tests.py"
+fi
+
 # Get working directory.
 if [ -f $DIR/../app/tests.py  ]
 then
-    location="$DIR/../app/tests.py"
+    location="$DIR/../app/$FILENAME"
 else
-    location="$DIR/../tests.py"
+    location="$DIR/../$FILENAME"
 fi
 
 # Run pytests...
